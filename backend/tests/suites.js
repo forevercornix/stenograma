8ef2702691cc9de315b0417b67df935e5b48af17
root <root@localhost.localdomain>
2026-09-08 23:40:08 +0300

    MATAVIMAS (laikinas): reference switch skaidymo formos

--- backend/tests/suites.js
+++ backend/tests/suites.js
@@ -370,6 +370,7 @@ const functional = [
   "restoredJobStoreForwarding",
   /** #157 (PR-4): bandymų registro modulis (grynas SQL sluoksnis). */
   "attemptRegistry",
+  "zzMatavimasCheck.integration",
   "sweepVerdiktai",
   /** #157 (PR-4): lygybės paritetas ir round-trip ištikimybė. */
   "artifactRoundTrip",
